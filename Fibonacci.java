db31441431018d5b706d26f8f82f70e365784e18
shahina <shahinashamsu1971@gmail.com>
2024-08-08 13:04:23 +0530

    Fibonacci program

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1, n3;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci terms to display: ");
+        int count = scanner.nextInt();
+
+        System.out.print(n1 + " " + n2);
+
+        for (int i = 2; i < count; ++i) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+
+        scanner.close();
+    }
+}
