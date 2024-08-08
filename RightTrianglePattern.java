79029c35a08ee0c5a500fba2eca7f6f644387caa
shahina <shahinashamsu1971@gmail.com>
2024-08-08 14:19:21 +0530

    Right triangle pattern program

---
+++ RightTrianglePattern.java
@@ -0,0 +1,14 @@
+public class RightTrianglePattern {
+    public static void main(String[] args) {
+        int rows = 6;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+ 
