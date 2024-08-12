b4ca0ed31e7fff362b4b6056a5e99cc20e8b2129
shahina <shahinashamsu1971@gmail.com>
2024-08-12 09:38:09 +0530

    Enumeration demo program

---
+++ EnumerationDemo.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class EnumerationDemo {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+
+
+        for (int i = 0; i <= 10; i++) {
+            v.addElement(i);
+        }
+
+       
+        System.out.println(v);
+
+       
+        Enumeration e = v.elements();
+        System.out.println("Even numbers in the vector:");
+
+        while (e.hasMoreElements()) {
+            Integer i = (Integer)e.nextElement();
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+        System.out.println(v);
+    }
+}
